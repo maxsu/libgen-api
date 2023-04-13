384da74573d683cdd95980130da670db6a38705a
Max Suica <max.suica@gmail.com>
2023-04-12 17:33:42 -0700

    test search: Use more descriptive names

--- test/test_search/test_search.py
+++ test/test_search/test_search.py
@@ -18,14 +18,16 @@ from .helpers import Filters, fields_match
 
 
 class TestBasicSearching:
-    def test_title_search(self, title: str, title_results: Request):
+    def test_search_by_title_contains_query(self, title: str, title_results: Request):
         first = title_results[0]
         assert title in first.title
 
-    def test_author_search(self, author: str, author_results: Results):
+    def test_search_by_author_contains_query(self, author: str, author_results: Results):
         assert all(author in item.author for item in author_results)
 
-    def test_resolve_download_links(self, author_results: Results):
+    def test_resolve_download_links_has_nonempty_urls_and_correct_keys(
+        self, author_results: Results
+    ):
         title_to_download = author_results[0]
         download_links = title_to_download.download_links
 
@@ -35,7 +37,7 @@ class TestBasicSearching:
         # download_links has keys from MIRROR_SOURCES
         assert list(download_links.keys()) == MIRROR_SOURCES
 
-    def test_raise_error_on_short_search(self, short_search: Request):
+    def test_raise_error_on_short_search_query(self, short_search: Request):
         # should return an error if search query is less than 3 characters long
         with pytest.raises(Exception):
             search(short_search)
@@ -43,14 +45,14 @@ class TestBasicSearching:
 
 class TestResultFiltering:
     class TestExactFiltering:
-        def test_title_filtering(self, title, title_results):
+        def test_filter_title_results_with_exact_match(self, title, title_results):
             titles = title_results.filter(Filters.year_extension, exact_match=True)
             first_result = titles[0]
 
             assert title in first_result.title
             assert fields_match(Filters.year_extension, first_result)
 
-        def test_author_filtering(self, author, author_results):
+        def test_filter_author_results_with_exact_match(self, author, author_results):
             filters = {"language": "German", "year": "2009"}
 
             titles = author_results.filter(filters, exact_match=True)
@@ -61,7 +63,7 @@ class TestResultFiltering:
 
         # explicit test of exact filtering
         # should return no results as they will all get filtered out
-        def test_exact_filtering(self, author_results):
+        def test_exact_filtering_returns_no_results_when_filter_has_bad_case(self, author_results):
             # this will filter out all results. "pdf" is lower case on Library Genesis
             titles = author_results.filter(Filters.inexact, exact_match=True)
             titles2 = author_results.filter(Filters.inexact2, exact_match=True)
@@ -69,7 +71,7 @@ class TestResultFiltering:
             assert len(titles2) == 0
 
     class TestInexactFiltering:
-        def test_inexact_filtering(self, author, author_results):
+        def testest_filter_author_results_with_inexact_match(self, author, author_results):
             titles = author_results.filter(Filters.inexact2, exact_match=False)
             first_result = titles[0]
 
@@ -77,14 +79,14 @@ class TestResultFiltering:
             assert fields_match(Filters.inexact2, first_result, exact=False)
 
     class TestPartialFiltering:
-        def test_inexact_partial_filtering(self, title, title_results):
+        def test_filter_title_results_with_inexact_partial_match(self, title, title_results):
             titles = title_results.filter(Filters.partial_inexact, exact_match=False)
             first_result = titles[0]
 
             assert title in first_result.title
             assert fields_match(Filters.partial_inexact, first_result, exact=False)
 
-        def test_exact_partial_filtering(self, title_results):
+        def test_exact_partial_filtering_returns_no_results_when_no_match(self, title_results):
             titles = title_results.filter(Filters.partial_exact, exact_match=True)
 
             assert len(titles) == 0
